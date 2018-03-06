fa8ac150ca7dd0b6541abc56f585ce8fb4b0434e
eirikgarsol <eirik.g.solberg@gmail.com>
2018-03-06 17:20:43 +0100

    Added librealsense

---
+++ rasberry_strawberry_picking/src/librealsense/unit-tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/isler/catkin_ws/src/librealsense/unit-tests
+# Build directory: /home/isler/catkin_ws/src/librealsense/unit-tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
