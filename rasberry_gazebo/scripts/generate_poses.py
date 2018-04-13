ff595d5baf227198a2ffe45c78eff9e88b08ac81
adambinch <adambinch@gmail.com>
2018-04-13 16:38:34 +0100

    Many simulation improvements including movable model humans ('actors').

---
+++ rasberry_gazebo/scripts/generate_poses.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 25 15:20:20 2018
+
+@author: Adam Binch
+@email: adambinch@gmail.com
+"""
+################################################################################
+from __future__ import division
+import numpy as np
+from itertools import product
+from utils import to_list
+
+
+
+def get_pole_poses(nx, ny, dx, dy, xoffset, yoffset):
+    xposes = np.arange(0, nx*dx, dx) + xoffset
+    yposes = np.arange(0, ny*dy, dy) + yoffset
+    poses = []
+    for i, j in list(product(range(nx), range(ny))):
+        pose = [xposes[i], yposes[j]]
+        poses.append(pose)
+    return poses, xposes, yposes
+  
+  
+    
+def get_tray_poses(nx, ny, dx, xoffset, yposes):
+    tray_length = ((nx-1) * dx) + 0.1
+    tray_xpose = tray_length/2 + xoffset - 0.05
+    tray_xposes = np.zeros(ny) + tray_xpose
+    return to_list(tray_xposes, yposes), tray_length
+
+
+
+def get_trayp2_poses(tray_length, pole_xoffset, pole_ny, pole_yposes):
+    trayp2_xpose = tray_length/2 + pole_xoffset - 0.05
+    trayp2_xposes = np.zeros(pole_ny*2) + trayp2_xpose
+    a = np.empty(pole_ny)
+    b = np.empty(pole_ny)
+    for i in range(pole_ny):
+        a[i] = pole_yposes[i] - 0.075
+        b[i] = pole_yposes[i] + 0.075
+    trayp2_yposes = np.hstack((a, b))            
+    return to_list(trayp2_xposes, trayp2_yposes)
+    
+    
+    
+def get_pot_poses(pole_xposes, pole_yposes):
+    pot_dx = 0.5
+    pot_xposes = np.arange(pole_xposes[0]+0.024, pole_xposes[1], pot_dx)
+    pot_poses = []
+    for i, j in list(product(range(len(pot_xposes)), range(len(pole_yposes)))):
+        pot_pose = [pot_xposes[i], pole_yposes[j]]
+        pot_poses.append(pot_pose)
+    return pot_poses, pot_xposes
+    
+    
+    
+def get_plant_poses(pot_xposes, pot_yposes):
+    plant1_poses = []
+    for i, j in list(product(range(len(pot_xposes)), range(len(pot_yposes)))):
+        plant1_pose = [pot_xposes[i]-0.005, pot_yposes[j]-0.38, 1.282, 0.785]
+        plant1_poses.append(plant1_pose)
+    
+    plant2_poses = []
+    for i, j in list(product(range(len(pot_xposes)), range(len(pot_yposes)))):
+        plant2_pose = [pot_xposes[i]-0.2725, pot_yposes[j]-0.2645, 1.408, 0.000]
+        plant2_poses.append(plant2_pose)    
+    return plant1_poses + plant2_poses
+    
+    
+    
+def get_arch_poses(arch_nx, arch_dx, arch_xoffset, pole_dy, pole_yposes):
+    arch_xposes = np.arange(0, arch_nx*arch_dx, arch_dx) + arch_xoffset
+    arch_yposes = np.zeros(arch_nx) + pole_yposes[0] - pole_dy
+    return to_list(arch_xposes, arch_yposes), arch_xposes, arch_yposes
+    
+    
+
+def get_canopy_poses(arch_xposes, arch_ypose, pole_dy):
+    canopy_xposes = np.arange(arch_xposes[0], arch_xposes[1], 0.5)
+    canopy_yposes = np.zeros(len(canopy_xposes)) + arch_ypose + (3*pole_dy) + 0.017
+    return to_list(canopy_xposes, canopy_yposes), canopy_xposes, canopy_yposes
+################################################################################
--- rasberry_gazebo/scripts/generate_world.py
+++ rasberry_gazebo/scripts/generate_world.py
@@ -3,109 +3,194 @@
 """
 Created on Wed Mar 14 11:36:00 2018
 
-@author: adam
+@author: Adam Binch
+@email: adambinch@gmail.com
 """
 ################################################################################
-from __future__ import division
-import numpy as np
-from itertools import product
-import json, xmltodict, yaml, argparse
+import argparse, xmltodict, os, rospkg
 from add_to_world import AddtoWorld
+from utils import load_data_from_yaml, load_data_from_xml
+from generate_poses import *
 
 
-def load_data_from_yaml(filename):
-    with open(filename, 'r') as f:
-        return yaml.load(f)
+save_world = True
 
+rospack = rospkg.RosPack()
+base_dir = rospack.get_path('rasberry_gazebo')
+#base_dir = os.getcwd()[:-8]
 
-def load_data_from_xml(filename):
-    with open(filename) as f:
-        doc = json.dumps(xmltodict.parse(f.read()))
-        return json.loads(doc)        
-    
-    
-def get_pole_poses(nx, ny, dx, dy, xoffset, yoffset):
-    
-    xposes = np.arange(0, nx*dx, dx) + xoffset
-    yposes = np.arange(0, ny*dy, dy) + yoffset
-    
-    poses = []
-    for i, j in list(product(range(nx), range(ny))):
-        pose = [xposes[i], yposes[j]]
-        poses.append(pose)
-    return poses, xposes, yposes
-################################################################################    
+world_f = base_dir + '/worlds/empty_grass.world'
+world_d = load_data_from_xml(world_f)
+world = AddtoWorld(world_d)
+
+default_model_config = base_dir + '/config/models_AB.yaml'
+default_actor_config = base_dir + '/config/actors_AB.yaml'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_file", type=str, default=default_model_config, help="filename")       
+parser.add_argument("--actor_file", type=str, default=default_actor_config, help="filename")
+args = parser.parse_args()
+model_config = load_data_from_yaml(args.model_file)
+actor_config = load_data_from_yaml(args.actor_file)
+################################################################################
 
 
 ################################################################################
-save_world = True
+# MODELS
 
-base_dir = '/home/adam/rasberry_ws/src/RASberry/rasberry_gazebo'
-default_config = base_dir + '/config/test.yaml'
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", type=str, default=default_config, help="filename")
-args = parser.parse_args()
-model_config = load_data_from_yaml(args.file)
-
-world_f = base_dir + '/worlds/empty.world'
-world_d = load_data_from_xml(world_f)
-world = AddtoWorld(world_d)
 
 for model in model_config:
     
     if model.keys()[0] == 'polytunnel':
-        pole_f = base_dir + '/models/unit_cylinder_1/model.sdf'
+        pole_f = base_dir + '/models/pole/model.sdf'
         tray_f = base_dir + '/models/tray/model.sdf'
+        trayp2_f = base_dir + '/models/tray_part2/model.sdf'
         arch_f = base_dir + '/models/dummy_arch/model.sdf'
+        canopy_f = base_dir + '/models/canopy/model.sdf'
+        pot_f = base_dir + '/models/pot/model.sdf'
+        plant_f = base_dir + '/models/plant/model.sdf'
+        plant2_f = base_dir + '/models/plant2/model.sdf'
+        
         
         pole_d = load_data_from_xml(pole_f)    
         tray_d = load_data_from_xml(tray_f)    
+        trayp2_d = load_data_from_xml(trayp2_f)
         arch_d = load_data_from_xml(arch_f)
+        canopy_d = load_data_from_xml(canopy_f)
+        pot_d = load_data_from_xml(pot_f)
+        plant_d = load_data_from_xml(plant_f)
+        plant2_d = load_data_from_xml(plant2_f)
         
-        polytunnels = model['polytunnel']        
+        
+        pole_count = 0
+        tray_count = 0
+        trayp2_count = 0  
+        pot_count = 0
+        plant_count = 0
+        plant2_count = 0
+        arch_count = 0
+        canopy_count = 0   
+        
+        
+        polytunnels = model['polytunnel']
         for i in range(len(polytunnels)):
-            nx = polytunnels[i]['nx']
-            ny = polytunnels[i]['ny']
-            dx = polytunnels[i]['dx']
-            dy = polytunnels[i]['dy']
-            xoffset = polytunnels[i]['xpose']
-            yoffset = polytunnels[i]['ypose']
-        
-            pole_poses, pole_xposes, pole_yposes = get_pole_poses(nx, ny, dx, dy, xoffset, yoffset) 
+            if polytunnels[i]['include']:            
             
-            tray_length = (nx-1) * dx
-            tray_xpose = tray_length/2 + xoffset
-            tray_xposes = np.zeros(ny) + tray_xpose
-            tray_poses = np.ndarray.tolist(np.vstack((tray_xposes, pole_yposes)).T) 
+                pole_nx = polytunnels[i]['pole_nx']
+                pole_ny = polytunnels[i]['pole_ny']
+                pole_dx = polytunnels[i]['pole_dx']
+                pole_dy = polytunnels[i]['pole_dy']
+                pole_xoffset = polytunnels[i]['pole_xoffset']
+                pole_yoffset = polytunnels[i]['pole_yoffset']
+                
+                arch_nx = polytunnels[i]['arch_nx']
+                arch_dx = polytunnels[i]['arch_dx']
+                arch_xoffset = polytunnels[i]['arch_xoffset']
+                
+                
             
-            arch_yposes = np.zeros(nx) + pole_yposes[0] - dy
-            arch_poses = np.ndarray.tolist(np.vstack((pole_xposes, arch_yposes)).T)
-            
-            world.add_poles(pole_d, pole_poses, i)
-            world.add_trays(tray_d, tray_poses, tray_length, i) 
-            world.add_arches(arch_d, arch_poses, i)  
+                if (pole_nx * pole_dx) > 0 and (pole_ny * pole_dy) > 0:
+                    pole_poses, pole_xposes, pole_yposes = get_pole_poses(pole_nx, pole_ny, pole_dx, pole_dy, pole_xoffset, pole_yoffset) 
+                    world.add_poles(pole_d, pole_poses, pole_count)
+                    pole_count += len(pole_poses)
+                
+                    tray_poses, tray_length = get_tray_poses(pole_nx, pole_ny, pole_dx, pole_xoffset, pole_yposes)
+                    world.add_trays(tray_d, tray_poses, tray_length, tray_count) 
+                    tray_count += len(tray_poses)
+                
+                    trayp2_poses = get_trayp2_poses(tray_length, pole_xoffset, pole_ny, pole_yposes)
+                    world.add_traysp2(trayp2_d, trayp2_poses, tray_length, trayp2_count)                 
+                    trayp2_count += len(trayp2_poses)
+                
+                    pot_poses, pot_xposes = get_pot_poses([pole_xposes[0], pole_xposes[-1]], pole_yposes) 
+                    world.add_pots(pot_d, pot_poses, pot_count)
+                    pot_count += len(pot_poses)
+                
+                    #plant_poses = get_plant_poses(pot_xposes, pole_yposes)
+                    #world.add_plants(plant_d, plant_poses, plant_count)  
+                    #plant_count += len(plant_poses)
+                    
+                    world.add_plants2(plant2_d, pot_poses, plant2_count)  
+                    plant2_count += len(pot_poses)
 
+
+                if (arch_nx * arch_dx) > 0:
+                    arch_poses, arch_xposes, arch_yposes = get_arch_poses(arch_nx, arch_dx, arch_xoffset, pole_dy, pole_yposes)
+                    world.add_arches(arch_d, arch_poses, arch_count)
+                    arch_count += len(arch_poses)                    
+                    
+                    canopy_poses, canopy_xposes, canopy_yposes = get_canopy_poses([arch_xposes[0], arch_xposes[-1]], arch_yposes[0], pole_dy)
+                    world.add_canopy(canopy_d, canopy_poses, canopy_count)
+                    canopy_count += len(canopy_poses)
+    
+   
+   
     if model.keys()[0] == 'riseholme_enclosure':
-
         riseholme_enclosure_f = base_dir + '/models/riseholme_enclosure/model.sdf'    
         riseholme_enclosure_d = load_data_from_xml(riseholme_enclosure_f)
         
         riseholme_enclosure = model['riseholme_enclosure']
         for i in range(len(riseholme_enclosure)):
-            xpose = riseholme_enclosure[i]['xpose']
-            ypose = riseholme_enclosure[i]['ypose']
-            world.add_riseholme_enclosure(riseholme_enclosure_d, [xpose, ypose], i)
+            if riseholme_enclosure[i]['include']:
+                
+                xpose = riseholme_enclosure[i]['xpose']
+                ypose = riseholme_enclosure[i]['ypose']
+                world.add_riseholme_enclosure(riseholme_enclosure_d, [xpose, ypose], i)
+                
+                
+                
+    if model.keys()[0] == 'food_handling_shed':
+        fhs_f = base_dir + '/models/food_handling_shed/model.sdf'  
+        fhs_floor_f = base_dir + '/models/fhs_floor/model.sdf' 
+        fhs_d = load_data_from_xml(fhs_f)
+        fhs_floor_d = load_data_from_xml(fhs_floor_f)
         
-  
-models = world.world_d['sdf']['world']['model']
-for i in range(len(models)):
+        fhs = model['food_handling_shed']
+        for i in range(len(fhs)):
+            if fhs[i]['include']:
+                
+                xpose = fhs[i]['xpose']
+                ypose = fhs[i]['ypose']
+                world.add_fhs(fhs_d, [xpose, ypose], i)
+                world.add_fhs_floor(fhs_floor_d, [xpose, ypose], i)
+                
+                            
+                            
+    if model.keys()[0] == 'world_name':  
+        world_name = model['world_name']              
+    else:
+        world_name = 'thorvald_AB.world' # default
+################################################################################
+
+
+
+################################################################################        
+# ACTORS
+
+
+actor_f = base_dir + '/models/actor/actor'
+actor_d = load_data_from_xml(actor_f)
+for actor in actor_config:
+    if actor['include']:
+        world.add_actors(actor_d, actor)
+################################################################################
+
+
+
+################################################################################        
+m = world.world_d['sdf']['world']['model']
+for i in range(len(m)):
     print "-----------------"
-    print "[%s]"%models[i]['@name']
+    print "[%s]"%m[i]['@name']
+    
+a = world.world_d['sdf']['world']['actor']
+for i in range(len(a)):
+    print "-----------------"
+    print "[%s]"%a[i]['@name']    
 
 if save_world:
-    fh = open(base_dir + '/worlds/thorvald_AB.world', "w")
+    fh = open(base_dir + '/worlds/' + world_name, "w")
     s_output = xmltodict.unparse(world.world_d, pretty=True)
     fh.write(s_output)
     fh.close()    
-#################################################################################
+################################################################################
