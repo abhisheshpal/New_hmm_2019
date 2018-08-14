a349d09d0660f76655be689e328cb3a073c04297
Marc Hanheide <marc@hanheide.de>
2018-08-14 17:14:20 +0100

    more univeraal launch files

---
+++ rasberry_bringup/tmule/thorvald_007.sh
@@ -0,0 +1,6 @@
+export CATKIN_WORKSPACE="$HOME/rasberry_ws"
+export USE_IMU=true
+export TMAP="riseholme"
+export MAP="$(rospack find rasberry_navigation)/maps/riseholme.yaml"
+export ROBOT_TYPE="thorvald_ii_4wd4ws_slim"
+export ROSBRIDGE_IP="10.8.0.4"
