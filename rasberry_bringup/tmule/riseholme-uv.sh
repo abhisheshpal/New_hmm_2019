4c507e2735085878566a6a8ebfe86b4ac35313f8
LCASABU02 <LCASABU02@lcas.group>
2018-09-25 16:53:08 +0100

    Topological map for uv-rig navigation in riseholme

---
+++ rasberry_bringup/tmule/riseholme-uv.sh
@@ -0,0 +1,8 @@
+export TMAP="riseholme-uv"
+export MAP="`readlink -f ../../rasberry_navigation/maps/riseholme.yaml`"
+export NOGOMAP="`readlink -f ../../rasberry_navigation/maps/riseholme_no_go.yaml`"
+export ROSBRIDGE_IP="10.8.0.4"
+export USE_SIM=true
+export GAZEBO_WORLD=riseholme
+export EKF_PUBLISH_TF=false
+export USE_IMU=false
