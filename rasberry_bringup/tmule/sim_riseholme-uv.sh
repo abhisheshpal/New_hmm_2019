50c216bd33ffb3372eb8163e31dadaf1a6ae4c81
gpdas <contact@gauthampdas.com>
2018-10-01 10:28:14 +0100

    uv-sim

---
+++ rasberry_bringup/tmule/sim_riseholme-uv.sh
@@ -0,0 +1,6 @@
+export TMAP="riseholme-uv"
+export MAP="`readlink -f ../../rasberry_navigation/maps/riseholme.yaml`"
+export NOGOMAP="`readlink -f ../../rasberry_navigation/maps/riseholme_no_go.yaml`"
+export USE_SIM=true
+export USE_IMU=false
+export EKF_PUBLISH_TF=false
