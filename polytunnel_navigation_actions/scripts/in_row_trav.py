e0d13271e4f540965101599ec307e5461ad6f9c5
jailander <jpfentanes@sagarobotics.com>
2019-05-31 14:15:07 +0200

    Debug messages

--- polytunnel_navigation_actions/scripts/in_row_trav.py
+++ polytunnel_navigation_actions/scripts/in_row_trav.py
@@ -274,6 +274,7 @@ class inRowTravServer(object):
 
 
         if np.abs(ang_diff) >= self.initial_heading_tolerance:
+            print "INITIAL ANg DIFF: ", np.abs(ang_diff)
             while np.abs(ang_diff) >= self.initial_heading_tolerance and not self.cancelled:
                 self._send_velocity_commands(0.0, 0.0, self.kp_ang_ro*ang_diff, consider_minimum_rot_vel=True)
                 rospy.sleep(0.05)
@@ -440,8 +441,8 @@ class inRowTravServer(object):
 
         
     def do_stop(self, timer):
-        print "do_stop"
         if not self.active:
+            print "do_stop"
             self._send_velocity_commands(0.0, 0.0, 0.0)
         
         
