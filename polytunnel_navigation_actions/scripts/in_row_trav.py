24b02fafda02fd2ccc6575ee6ad0bdbdea314cb9
jailander <jpfentanes@sagarobotics.com>
2019-06-05 15:15:26 +0200

    less spam smaller cone

--- polytunnel_navigation_actions/scripts/in_row_trav.py
+++ polytunnel_navigation_actions/scripts/in_row_trav.py
@@ -244,7 +244,7 @@ class inRowTravServer(object):
 
 
         if min_obs_dist <= self.approach_dist_to_obj:
-            if (np.abs(obs_ang) <= (np.pi/20.0)) or (np.abs(obs_ang) >= (np.pi*19/20.0)): 
+            if (np.abs(obs_ang) <= (np.pi/25.0)) or (np.abs(obs_ang) >= (np.pi*24/25.0)): 
                 if (obs_ang < np.pi/2.0 or obs_ang > -np.pi/2.0) and not self.backwards_mode:
                     #print "Obstacle Size: ", obs.radius, " detected at ", obs_ang, " degrees ", obs_dist," meters away",  self.backwards_mode           
                     self.object_detected = True
@@ -451,13 +451,13 @@ class inRowTravServer(object):
     def get_forward_speed(self):
         if not self.constant_forward_speed:
             if not self.object_detected and self.curr_distance_to_object <= self.approach_dist_to_obj:
-                print "not limiting"
+                #print "not limiting"
                 if self.backwards_mode:
                     speed = -self.forward_speed
                 else:
                     speed = self.forward_speed
             else:
-                print "limiting"
+                #print "limiting"
                 slowdown_delta=self.approach_dist_to_obj-self.min_dist_to_obj
                 current_percent = (self.curr_distance_to_object-self.min_dist_to_obj)/slowdown_delta
                 if current_percent >0:
@@ -469,7 +469,7 @@ class inRowTravServer(object):
     #            else:
     #                speed = self.forward_speed
         else:
-            print "not limiting"
+            #print "not limiting"
             if self.backwards_mode:
                 speed = -self.forward_speed
             else:
