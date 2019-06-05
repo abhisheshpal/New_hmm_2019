78a94e7c1de631063550242de1eb04f54c691c77
jailander <jpfentanes@sagarobotics.com>
2019-06-05 14:29:19 +0200

    blah

--- polytunnel_navigation_actions/scripts/in_row_trav.py
+++ polytunnel_navigation_actions/scripts/in_row_trav.py
@@ -229,7 +229,6 @@ class inRowTravServer(object):
         min_obs_dist=1000
         for obs in msg.obstacles:
             if obs.radius > self.min_obj_size:
-                print "Obstacle Size: ", obs.radius, " detected at ", obs_ang, " degrees ", obs_dist," meters away",  self.backwards_mode
                 obs_pose = self._transform_to_pose_stamped(obs)
                 cobs_dist = np.hypot(obs_pose.pose.position.x, obs_pose.pose.position.y)
                 if cobs_dist < min_obs_dist:
@@ -237,7 +236,7 @@ class inRowTravServer(object):
                     min_obs_dist = obs_dist
                     obs_ang = math.atan2(obs_pose.pose.position.y, obs_pose.pose.position.x)
                     obs_ang = (obs_ang + 2*np.pi)%(2*np.pi)
-
+                    print "Obstacle Size: ", obs.radius, " detected at ", obs_ang, " degrees ", obs_dist," meters away",  self.backwards_mode
 
 
         if min_obs_dist <= self.approach_dist_to_obj:
