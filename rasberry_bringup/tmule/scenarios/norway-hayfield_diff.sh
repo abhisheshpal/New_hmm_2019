3f3eec22d849c24b9ce8ec2e1c6d9f8f341550f9
RASbery <rasberry@rasberry-server.novalocal>
2019-06-05 14:02:20 +0000

    scneario load fix

--- rasberry_bringup/tmule/scenarios/norway-hayfield_diff.sh
+++ rasberry_bringup/tmule/scenarios/norway-hayfield_diff.sh
@@ -7,4 +7,5 @@ export RTK_IP="192.168.0.20"
 export RTK_PORT="21098"
 export USE_OMNI=false
 export LOC_EDGE_RECONF="norway"
-export MIMIC_DES_CONFIG=$(rospack find rasberry_des)/config/mimic_norway_des_tworows.yaml
+#export MIMIC_DES_CONFIG=$(rospack find rasberry_des)/config/mimic_norway_des_tworows.yaml
+export MIMIC_DES_CONFIG="`readlink -f ../../rasberry_des/config/mimic_norway_des_tworows.yaml`"
